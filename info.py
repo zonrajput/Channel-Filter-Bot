0ea16c601fb9dcc6835b436b5fe0ce41ca8b5c06
zonrajput <128221996+zonrajput@users.noreply.github.com>
2023-04-21 13:17:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 26773184
+API_HASH     = "2bd258672b1a5008c788d92ff37904a0"
+BOT_TOKEN    = "6262576023:AAF6zs6rJkRw2Ro_BeeqDVAyCma-CAYEhAM"
+SESSION      = "BQGYhsAAQFrjnQ9-riyUSTZ-kaCPPG9UK7uiTsNXTE7zFhZZtRJl546tqM4ZsunVqroEpVnkzbV-otuInt9aDZxbt9y_NWaY0pZTXUxqagm4cUGQwBc9KLlxhtEziPqb3qZf3_mJp3pPjdYQqqX7_9N2ETTY6hLnGLypw_7LOVxq9Uw0XaSa6L90MH--mYFYu-T93sfYqmsDTOx2Ms_S88k3JqADChvjjd17UmHgb04GJ5aU-Ny9cm-XWEHrelmH2hRoxSSw3H0CLmmcizGQRek0lbcSCbT5HItMplv3BkL77NS7PgOYqcYUSHbj6EHs7s_h5Zbh6tDWGEhNnz3QFB9T7tlRmAAAAABsrZSYAA"
+DATABASE_URI = "mongodb+srv://jonesanatan:jon897906@cluster0.pa8ej9n.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001857178947
+ADMIN        = 1823315096
+CHANNEL      = "@movizoneh"
